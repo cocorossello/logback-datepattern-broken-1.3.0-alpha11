c65f8223b7f45ec5b79ec4d5906dd6974faefc80
vrossello <vrossello@travelcompositor.com>
2021-12-18 00:20:54 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+Instructions:
+
+Run tests:
+
+`./gradlew test`
+
+With logback 1.3.0-alpha11 output looks like:
+
+`20 ene 1970;00:29:43.126 INFO  org.example.DatePatternBrokenTest - something`
+
+
+Switching to logback 1.3.0-alpha10, output looks like:
+
+`18 dic 2021;00:19:58.343 INFO  org.example.DatePatternBrokenTest - something`
---
+++ src/test/java/org/example/DatePatternBrokenTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+public class DatePatternBrokenTest {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(DatePatternBrokenTest.class);
+
+
+    @Test
+    public void log_something() {
+        LOGGER.info("something");
+    }
+
+}
